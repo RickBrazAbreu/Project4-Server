4680a3f64828de4f802924cfbbf53d75267f4a6e
Pedro Abreu <henriqueibtou@gmail.com>
2022-08-19 12:39:57 -0700

    Keep adding seeds

---
+++ app/models/seed.js
@@ -0,0 +1,8 @@
+const startItens = [
+    { name: 'T-shirt', brand: 'Adidas' , type: 'Cotton', price: 20 },
+    { name: 'Shorts', brand: 'Adidas' , type: 'Cotton', price: 35 },
+    { name: 'Pants', brand: 'Puma' , type: 'Jeans', price: 40 },
+    { name: 'Sweater', brand: 'Nike' , type: 'Cotton', price: 55 },
+    { name: 'Shoes', brand: 'Nike' , type: 'Leather', price: 20 }
+]
+export default startItens
